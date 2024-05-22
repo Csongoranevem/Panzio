fda74ced348f29292ac12e8585414e93b7df9288
Csongorka2013 <czakocsongor@turr.hu>
2024-05-22 11:35:03 +0200

    3. óra js folytatása

--- foglalas.js
+++ foglalas.js
@@ -4,19 +4,51 @@ function Kalkulacio() {
     let erkdat = new Date(document.getElementById("erkezes").value)
     let tavdat = new Date(document.getElementById("tavozas").value)
     let idotartam = (((+tavdat)-(+erkdat)) / 3600000)/24;
+    let vendegsz=document.getElementById("vendegszam").value
     //Szoba típus
-    szobatipK = document.getElementsByName("rad2");
+    let szobatipK = document.getElementsByName("rad2");
     let szobatip;
     for (let i = 0; i < szobatipK.length; i++) {
-        if (szobatipK[i].checked ==true){
-            szobatip = szobatipK[i]
+        if (szobatipK[i].checked){
+            szobatip = szobatipK[i].value
+            break;
+        }
+        
+    }
+    let ellatasK = document.getElementsByName("rad");
+    let ellatas;
+    for (let i = 0; i < ellatasK.length; i++) {
+        if (ellatasK[i].checked){
+            ellatas = ellatasK[i].value
             break;
         }
         
     }
 
-    alert(erkdat, );
+    let szolgaltatasK = document.getElementsByClassName("chbox")
+    let szolgaltatas;
+    let szolgfull="";
+    for (let i = 0; i < array.length; i++) {
+        if (szolgaltatasK[i].checked) {
+            szolgaltatas = szolgaltatasK[i].value;
+            szolgfull +=szolgaltatas+"\n"
+        }
+        
+    }
 
+            //Kiiras
+            alert(
+                "Kedves Vendégünk!\n\n"+
+                "Tájékoztatjuk a sikeres foglalásáról.\n\n"+
+                "Érkezés: " + erkdat.getFullYear()+"."+ (erkdat.getMonth()+1)+"."+(erkdat.getDate())+"\n"+
+                "Távozás: "+ tavdat.getFullYear()+"."+ (tavdat.getMonth()+1)+"."+(tavdat.getDate())+"\n"+
+                "Szoba típusa: "+ szobatip+"\n"+
+                "Vendégek száma: "+vendegsz+"\n"+
+                "Ellátás: "+ellatas+"\n"+
+                "Igényelt szolgáltatások: "+szolgfull
+
+            )
+            console.log(ellatas)
 
     
 }
